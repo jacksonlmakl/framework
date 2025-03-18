a8a582e8acecac811a38f813845a9e795ea65599
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2025-03-18 15:15:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -308,7 +308,7 @@ const LogsModal = () => {
   };
 
   const deployFlow = () => {
-    executeTerminalCommand('bash docker-deploy-wrapper', 'Flow deployed successfully');
+    executeTerminalCommand('bash bin/deploy', 'Flow deployed successfully');
   };
 
   const stopFlow = () => {
